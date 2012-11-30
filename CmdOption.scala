4c60594e6b703acc4e719edf28a8775dc6aa24fc
Tobias Roeser <le.petit.fou@web.de>
2012-11-30 08:40:00 +0000

    Added the Maven Central repo release process to the build scripts.

--- CmdOption.scala
+++ CmdOption.scala
@@ -1,5 +1,5 @@
 object CmdOption {
 
-  val version = "0.2.0"
+  val version = "0.2.1-SNAPSHOT"
 
 }
--- SBuild.scala
+++ SBuild.scala
@@ -47,4 +47,74 @@ class SBuild(implicit project: Project) {
     )
   }
 
+  Target("phony:prepareMvnStaging") dependsOn binDistFiles.filter(_.endsWith(".jar")).map{ f => TargetRef(f) } exec { ctx: TargetContext =>
+    AntMkdir(dir = Path("target/mvn"))
+
+    val pom = """<project>
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>de.tototec</groupId>
+  <artifactId>de.tototec.cmdoption</artifactId>
+  <packaging>jar</packaging>
+  <version>""" + CmdOption.version + """</version>
+  <name>CmdOption</name>
+  <description>CmdOption is a simple annotation-driven command line parser toolkit for Java 5 applications that is configured through annotations.</description>
+  <url>http://cmdoption.tototec.de</url>
+  <licenses>
+    <license>
+      <name>The Apache Software License, Version 2.0</name>
+      <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+      <distribution>repo</distribution>
+    </license>
+  </licenses>
+  <scm>
+    <url>http://cmdoption.tototec.de/svn/cmdoption</url>
+    <connection>http://cmdoption.tototec.de/svn/cmdoption</connection>
+  </scm>
+  <developers>
+    <developer>
+      <id>TobiasRoeser</id>
+      <name>Tobias Roeser</name>
+      <email>tobias.roeser@tototec.de</email>
+    </developer>
+  </developers>
+</project>"""
+
+    AntEcho(message = pom, file = Path("target/mvn/pom.xml"))
+
+    ctx.fileDependencies.foreach { file =>
+      AntCopy(file = file, toDir = Path("target/mvn"))
+    }
+
+    val script = """#!/bin/sh
+
+echo "Please edit settings.xml with propper connection details."
+read
+
+echo "Uploading jar"
+mvn -s ./settings.xml gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=de.tototec.cmdoption-""" + CmdOption.version + """.jar -Durl=http://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+
+echo "Uploading sources"
+mvn -s ./settings.xml gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=de.tototec.cmdoption-""" + CmdOption.version + """-sources.jar -Dclassifier=sources -Durl=http://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+
+echo "Uploading javadoc"
+mvn -s ./settings.xml gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=de.tototec.cmdoption-""" + CmdOption.version + """-javadoc.jar -Dclassifier=javadoc -Durl=http://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+"""
+
+    AntEcho(message = script, file = Path("target/mvn/script.sh"))
+
+    val settings = """<settings>
+  <servers>
+    <server>
+      <id>sonatype-nexus-staging</id>
+      <username>your-username</username>
+      <password>your-password</password>
+    </server>
+  </servers>
+</settings>
+"""
+
+    AntEcho(message = settings, file = Path("target/mvn/settings.xml"))
+
+  }
+
 }
