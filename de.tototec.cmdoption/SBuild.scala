9853bb6bfc9d0b32cfc04b3f2ddf6f5c6a041b0c
Tobias Roeser <le.petit.fou@web.de>
2013-04-22 07:23:30 +0000

    Updated SBuild.scala.

--- de.tototec.cmdoption/SBuild.scala
+++ de.tototec.cmdoption/SBuild.scala
@@ -3,75 +3,67 @@ import de.tototec.sbuild.TargetRefs._
 import de.tototec.sbuild.ant._
 import de.tototec.sbuild.ant.tasks._
 
-@version("0.2.0")
+@version("0.4.0")
 @include("../CmdOption.scala")
-@classpath("http://repo1.maven.org/maven2/org/apache/ant/ant/1.8.4/ant-1.8.4.jar")
+@classpath("mvn:org.apache.ant:ant:1.8.4")
 class SBuild(implicit _project: Project) {
 
   val version = CmdOption.version
 
-  val jar = "target/de.tototec.cmdoption-" + version + ".jar"
+  val jar = s"target/de.tototec.cmdoption-$version.jar"
   val sourcesJar = jar.substring(0, jar.length - 4) + "-sources.jar"
   val javadocJar = jar.substring(0, jar.length - 4) + "-javadoc.jar"
-
-  SchemeHandler("mvn", new MvnSchemeHandler())
 
   val testCp = "mvn:org.testng:testng:6.4" ~
     "mvn:com.beust:jcommander:1.30"
 
   ExportDependencies("eclipse.classpath", testCp)
 
-  val javaFiles = (Path("src/main/java/de/tototec/cmdoption").listFiles ++
-    Path("src/main/java/de/tototec/cmdoption/handler").listFiles).
-    filter(_.getName.endsWith(".java"))
-
   val poFiles = Path("src/main/po").listFiles.filter(f => f.getName.endsWith(".po"))
 
   Target("phony:all") dependsOn jar ~  sourcesJar ~ "test"
 
-  Target("phony:clean") exec {
+  Target("phony:clean").evictCache exec {
     AntDelete(dir = Path("target"))
   }
 
-  def compileJava(sources: java.io.File, destDir: java.io.File, ctx: TargetContext) = {
-    IfNotUpToDate(sources, Path("target"), ctx) {
-      AntMkdir(dir = destDir)
-      AntJavac(source = "1.5", target = "1.5", encoding = "UTF-8",
-        debug = true, fork = true, includeAntRuntime = false,
-        classpath = AntPath(locations = ctx.fileDependencies),
-        srcDir = AntPath(sources),
-        destDir = destDir
-      )
-    }
+  Target("phony:compile").cacheable dependsOn "scan:src/main/java" exec {
+    addons.java.Javac(
+      source = "1.5", target = "1.5", encoding = "UTF-8", debugInfo = "all",
+      destDir = Path("target/classes"),
+      sources = "scan:src/main/java".files
+    )
   }
 
-  Target("phony:compile") exec { ctx: TargetContext =>
-    compileJava(sources = Path("src/main/java"), destDir = Path("target/classes"), ctx = ctx)
-  }
-
-  Target("phony:compileTest") dependsOn testCp ~ jar exec { ctx: TargetContext =>
-    compileJava(sources = Path("src/test/java"), destDir = Path("target/test-classes"), ctx = ctx)
+  Target("phony:compileTest").cacheable dependsOn testCp ~ jar ~ "scan:src/test/java" exec {
+    addons.java.Javac(
+      source = "1.5", target = "1.5", encoding = "UTF-8", debugInfo = "all",
+      destDir = Path("target/test-classes"),
+      sources = "scan:src/test/java".files,
+      classpath = testCp.files ++ jar.files
+    )
   }
 
   Target("phony:test") dependsOn "compileTest" ~ testCp ~ jar exec { ctx: TargetContext =>
     val tests = Seq(
       "de.tototec.cmdoption.DelegateTest",
       "de.tototec.cmdoption.ExampleSemVerTest",
+      "de.tototec.cmdoption.FieldInheritanceTest",
       "de.tototec.cmdoption.ParserTest",
       "de.tototec.cmdoption.TranslationTutorialMain",
       "de.tototec.cmdoption.handler.UrlHandlerTest"
     )
 
     AntJava(
-      failOnError = true, 
+      failOnError = true,
       classpath = AntPath(locations = ctx.fileDependencies ++ Seq(Path("target/test-classes"), Path("src/test/resources"))),
-      className = "org.testng.TestNG", 
+      className = "org.testng.TestNG",
       arguments = Seq("-testclass", tests.mkString(","), "-d", Path("target/test-output").getPath))
   }
 
   val msgCatalog = Path("target/po/messages.pot")
 
-  Target(msgCatalog) dependsOn javaFiles.map { TargetRef(_) }.toSeq exec { ctx: TargetContext =>
+  Target(msgCatalog) dependsOn "scan:src/main/java" exec { ctx: TargetContext =>
     AntMkdir(dir = ctx.targetFile.get.getParentFile)
 
     import java.io.File
@@ -84,7 +76,7 @@ class SBuild(implicit _project: Project) {
         "-ktr", "-kmarktr",
         "--directory", new File(srcDirUri).getPath,
         "--output-dir", ctx.targetFile.get.getParent,
-        "--output", ctx.targetFile.get.getName) ++ ctx.fileDependencies.map(file => srcDirUri.relativize(file.toURI).getPath)
+        "--output", ctx.targetFile.get.getName) ++ "scan:src/main/java".files.map(file => srcDirUri.relativize(file.toURI).getPath)
     )
   }
 
