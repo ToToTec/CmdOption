7eaddc34e30266efd972058d93eede86f5d93c0a
Tobias Roeser <le.petit.fou@web.de>
2017-10-09 13:31:11 +0200

    Updated translation build process
    Removed maven profile

--- de.tototec.cmdoption/pom.scala
+++ de.tototec.cmdoption/pom.scala
@@ -92,31 +92,19 @@ Model(
             phase = "process-sources",
             goals = Seq("run"),
             configuration = Config(
-              target = Config(
-                mkdir = Config(`@dir` = "${project.basedir}/target/po"),
-                // run xgettext on the source files
-                apply = Config(
-                  `@verbose` = "true",
-                  `@failOnError` = "true",
-                  `@executable` = "xgettext",
-                  // only one invocation with all files
-                  `@parallel` = "true",
-                  // ensure, we pass the files relative to their fileset roots
-                  `@relative` = "true",
-                  arg = Config(`@value` = "-ktr"),
-                  arg = Config(`@value` = "-kmarktr"),
-                  arg = Config(`@value` = "-kpreparetr"),
-                  arg = Config(`@value` = "--sort-by-file"),
-                  arg = Config(`@value` = "--directory"),
-                  arg = Config(`@value` = "src/main/java"),
-                  arg = Config(`@value` = "--output-dir"),
-                  arg = Config(`@value` = "${project.basedir}/target/po"),
-                  arg = Config(`@value` = "--output"),
-                  arg = Config(`@value` = "messages.pot"),
-                  // the source files to scan
-                  fileset = Config(`@dir` = "${project.basedir}/src/main/java")
+              target = Gettext.extractMessagesTarget
+            )
+          ),
+          // Update/merge translations with current message catalog
+          // Not bound to life-cycle
+          // Please run manually with: mvn antrun:run@update-translations
+          Execution(
+            id = "update-translations",
+            goals = Seq("run"),
+            configuration = Config(
+              target = new Config(
+                  Gettext.extractMessagesTarget.elements ++ Gettext.mergeMessagesTarget.elements
                 )
-              ) //< target
             )
           ),
           // Generate properties files for translations
@@ -125,33 +113,7 @@ Model(
             phase = "process-resources",
             goals = Seq("run"),
             configuration = Config(
-              target = Config(
-                mkdir = Config(`@dir` = "${project.basedir}/target/classes/de/tototec/cmdoption"),
-                // run msgmerge on the translation files to generate property files
-                apply = Config(
-                  `@executable` = "msgmerge",
-                  `@verbose` = "true",
-                  `@failOnError` = "true",
-                  // one invocation for each translation files
-                  `@parallel` = "false",
-                  `@dest` = "${project.basedir}/target/classes/de/tototec/cmdoption",
-                  // ensure, we pass the files relative to their fileset roots
-                  arg = Config(`@value` = "--output-file"),
-                  // marker for the target file position
-                  targetfile = Config(`@suffix` = ""),
-                  arg = Config(`@value` = "--properties-output"),
-                  // marker for the source file position
-                  srcfile = Config(`@suffix` = ""),
-                  // source files  
-                  fileset = Config(`@dir` = "${project.basedir}/src/main/po", `@includes` = "*.po"),
-                  arg = Config(`@value` = "${project.basedir}/target/po/messages.pot"),
-                  mapper = Config(
-                    `@type` = "glob",
-                    `@from` = "*.po",
-                    `@to` = "Messages_*.properties"
-                  )
-                )
-              ) //< target
+              target = Gettext.generatePropertiesTarget
             )
           )
         )
@@ -159,42 +121,6 @@ Model(
     )
   ),
   profiles = Seq(
-    Profile(
-      id = "update-messages",
-      build = BuildBase(
-        plugins = Seq(
-          // Update/merge translations with current message catalog
-          Plugin(
-            gav = "org.apache.maven.plugins" % "maven-antrun-plugin" % "1.8",
-            executions = Seq(
-              Execution(
-                id = "merge-messages",
-                phase = "generate-resources",
-                goals = Seq("run"),
-                configuration = Config(
-                  target = Config(
-                    // run msgmerge on the translation files to update them
-                    apply = Config(
-                      `@executable` = "msgmerge",
-                      `@verbose` = "true",
-                      `@failOnError` = "true",
-                      // one invocation for each translation files
-                      `@parallel` = "false",
-                      arg = Config(`@value` = "--backup=none"),
-                      // ensure, we pass the files relative to their fileset roots
-                      arg = Config(`@value` = "--update"),
-                      srcfile = Config(`@suffix` = ""),
-                      fileset = Config(`@dir` = "${project.basedir}/src/main/po", `@includes` = "*.po"),
-                      arg = Config(`@value` = "${project.basedir}/target/po/messages.pot")
-                    )
-                  ) //< target
-                )
-              )
-            )
-          )
-        )
-      )
-    ),
     genPomXmlProfile
   )
 )
--- mvn-shared.scala
+++ mvn-shared.scala
@@ -5,38 +5,112 @@ object CmdOption {
 }
 
 val genPomXmlProfile = Profile(
-      id = "gen-pom-xml",
-      build = BuildBase(
-        plugins = Seq(
-          // Generate pom.xml from pom.scala
-          Plugin(
-            gav = "io.takari.polyglot" % "polyglot-translate-plugin" % "0.2.1",
-            executions = Seq(
-              Execution(
-                id = "pom-scala-to-pom-xml",
-                phase = "initialize",
-                goals = Seq("translate-project"),
-                configuration = Config(
-                  input = "pom.scala",
-                  output = "pom.xml"
-                )
-              )
-            )
-          ),
-          // Clean generated pom.xml
-          Plugin(
-            gav = "org.apache.maven.plugins" % "maven-clean-plugin" % "3.0.0",
+  id = "gen-pom-xml",
+  build = BuildBase(
+    plugins = Seq(
+      // Generate pom.xml from pom.scala
+      Plugin(
+        gav = "io.takari.polyglot" % "polyglot-translate-plugin" % "0.2.1",
+        executions = Seq(
+          Execution(
+            id = "pom-scala-to-pom-xml",
+            phase = "initialize",
+            goals = Seq("translate-project"),
             configuration = Config(
-              filesets = Config(
-                fileset = Config(
-                  directory = "${basedir}",
-                  includes = Config(
-                    include = "pom.xml"
-                  )
-                )
+              input = "pom.scala",
+              output = "pom.xml"
+            )
+          )
+        )
+      ),
+      // Clean generated pom.xml
+      Plugin(
+        gav = "org.apache.maven.plugins" % "maven-clean-plugin" % "3.0.0",
+        configuration = Config(
+          filesets = Config(
+            fileset = Config(
+              directory = "${basedir}",
+              includes = Config(
+                include = "pom.xml"
               )
             )
           )
         )
       )
     )
+  )
+)
+
+object Gettext {
+
+  def extractMessagesTarget: Config = Config(
+    mkdir = Config(`@dir` = "${project.basedir}/target/po"),
+    // run xgettext on the source files
+    apply = Config(
+      `@verbose` = "true",
+      `@failOnError` = "true",
+      `@executable` = "xgettext",
+      // only one invocation with all files
+      `@parallel` = "true",
+      // ensure, we pass the files relative to their fileset roots
+      `@relative` = "true",
+      arg = Config(`@value` = "-ktr"),
+      arg = Config(`@value` = "-kmarktr"),
+      arg = Config(`@value` = "-kpreparetr"),
+      arg = Config(`@value` = "--sort-by-file"),
+      arg = Config(`@value` = "--directory"),
+      arg = Config(`@value` = "src/main/java"),
+      arg = Config(`@value` = "--output-dir"),
+      arg = Config(`@value` = "${project.basedir}/target/po"),
+      arg = Config(`@value` = "--output"),
+      arg = Config(`@value` = "messages.pot"),
+      // the source files to scan
+      fileset = Config(`@dir` = "${project.basedir}/src/main/java")
+    )
+  )
+
+  def mergeMessagesTarget: Config = Config(
+    // run msgmerge on the translation files to update them
+    apply = Config(
+      `@executable` = "msgmerge",
+      `@verbose` = "true",
+      `@failOnError` = "true",
+      // one invocation for each translation files
+      `@parallel` = "false",
+      arg = Config(`@value` = "--backup=none"),
+      // ensure, we pass the files relative to their fileset roots
+      arg = Config(`@value` = "--update"),
+      srcfile = Config(`@suffix` = ""),
+      fileset = Config(`@dir` = "${project.basedir}/src/main/po", `@includes` = "*.po"),
+      arg = Config(`@value` = "${project.basedir}/target/po/messages.pot")
+    )
+  )
+
+  def generatePropertiesTarget: Config = Config(
+    mkdir = Config(`@dir` = "${project.basedir}/target/classes/de/tototec/cmdoption"),
+    // run msgmerge on the translation files to generate property files
+    apply = Config(
+      `@executable` = "msgmerge",
+      `@verbose` = "true",
+      `@failOnError` = "true",
+      // one invocation for each translation files
+      `@parallel` = "false",
+      `@dest` = "${project.basedir}/target/classes/de/tototec/cmdoption",
+      // ensure, we pass the files relative to their fileset roots
+      arg = Config(`@value` = "--output-file"),
+      // marker for the target file position
+      targetfile = Config(`@suffix` = ""),
+      arg = Config(`@value` = "--properties-output"),
+      // marker for the source file position
+      srcfile = Config(`@suffix` = ""),
+      // source files  
+      fileset = Config(`@dir` = "${project.basedir}/src/main/po", `@includes` = "*.po"),
+      arg = Config(`@value` = "${project.basedir}/target/po/messages.pot"),
+      mapper = Config(
+        `@type` = "glob",
+        `@from` = "*.po",
+        `@to` = "Messages_*.properties"
+      )
+    )
+  )
+}
