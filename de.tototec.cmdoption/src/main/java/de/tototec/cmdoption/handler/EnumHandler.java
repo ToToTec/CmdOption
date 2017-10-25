fc7801e4a686524184b9156bbc7e3e709fc9c610
Tobias Roeser <le.petit.fou@web.de>
2017-10-25 14:13:03 +0200

    Fixed typo

--- de.tototec.cmdoption/src/main/java/de/tototec/cmdoption/handler/EnumHandler.java
+++ de.tototec.cmdoption/src/main/java/de/tototec/cmdoption/handler/EnumHandler.java
@@ -13,7 +13,7 @@ import de.tototec.cmdoption.internal.I18n.PreparedI18n;
 import de.tototec.cmdoption.internal.I18nFactory;
 
 /**
- * Parse a Sting to a {@link Enum} of the expected type and applies it to a
+ * Parse a String to a {@link Enum} of the expected type and applies it to a
  * field or a one-arg method. The <code>Enum.valueOf</code> method is used.
  *
  * @since 0.4.2
